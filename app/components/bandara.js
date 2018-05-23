3118a3e607f0023c54fa1ce636b31740da754872
fannyhasbi <fnyhsbi@gmail.com>
2018-05-23 10:54:54 +0700

    Add list to bandara

--- app/components/bandara.js
+++ app/components/bandara.js
@@ -2,49 +2,43 @@ import React from 'react';
 import { Text, View, Button, StyleSheet } from 'react-native';
 import {List, ListItem} from 'react-native-elements';
 
+import Title from './title';
+
 export default class BandaraScreen extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      data_bandara = [
+      data_bandara : [
         {
-          nama: 'Bandar Udara Soekarno Hatta',
+          nama: 'A',
           tempat: 'Jakarta'
         },
         {
-          nama: 'Bandar Udara Ahmad Yani',
+          nama: 'B',
           tempat: 'Semarang'
         }
-      ];
-    }
+      ]
+    };
   }
 
   render(){
     return (
       <View>
-        <Text style={styles.header}>Daftar Bandara</Text>
+        <Title text="Daftar Bandara" />
         
         <List>
-        {
-          this.state.map((l, i)) => (
-            <ListItem
-              key={i}
-              title={l.nama}
-              subtitle={l.tempat}
-            />
-          );
-        }
+          {
+            this.state.data_bandara.map((l, i) => (
+              <ListItem
+                key={i}
+                title={l.nama}
+                subtitle={l.tempat}
+              />
+            ))
+          }
         </List>
+        
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  header: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: 20
-  }
-});
--- app/components/title.js
+++ app/components/title.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-// import {Text} from 'react-native-elements';
+import {View, StyleSheet} from 'react-native';
+import {Text} from 'react-native-elements';
 
 export default class Title extends React.Component {
   render(){
     return (
-      <Text style={styles.header}>{this.props.text}</Text>
+      <View style={styles.header}>
+        <Text h3>{this.props.text}</Text>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   header: {
-    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    paddingTop: 20
+    paddingTop: 25
   }
-});
+})
