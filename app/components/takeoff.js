2c805632de2bc8d1ca77a6d8484fc5725b241c84
fannyhasbi <fnyhsbi@gmail.com>
2018-05-23 11:37:37 +0700

    Add takeoff screen

--- app/components/takeoff.js
+++ app/components/takeoff.js
@@ -1,12 +1,56 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import {List, ListItem} from 'react-native-elements';
+import {createStackNavigator} from 'react-navigation';
 
-export default class TakeOffScreen extends React.Component {
+class TakeOff extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      data_takeoff : [
+        {
+          tujuan: 'A',
+          waktu: '2016',
+          kode_pesawat: 'asc',
+          penumpang: 126
+        },
+        {
+          tujuan: 'B',
+          waktu: '2017',
+          kode_pesawat: 'das',
+          penumpang: 324
+        }
+      ]
+    };
+  }
+
   render(){
     return (
       <View>
-        <Text>Menu takeoff</Text>
+        <List>
+          {
+            this.state.data_takeoff.map((l, i) => (
+              <ListItem
+                key={i}
+                title={l.tujuan}
+                subtitle={l.kode_pesawat}
+              />
+            ))
+          }
+        </List>
+        
       </View>
     );
   }
 }
+
+const TakeOffScreen = createStackNavigator({
+  TakeOff: {
+    screen: TakeOff,
+    navigationOptions: {
+      title: 'Data Take Off'
+    }
+  }
+});
+
+export default TakeOffScreen;
