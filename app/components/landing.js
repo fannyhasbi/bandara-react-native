3715fc02d6d30eff7056fe35823f880f4ec2bf18
fannyhasbi <fnyhsbi@gmail.com>
2018-05-23 11:41:11 +0700

    Add landing screen

--- app/components/landing.js
+++ app/components/landing.js
@@ -1,12 +1,56 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import {List, ListItem} from 'react-native-elements';
+import {createStackNavigator} from 'react-navigation';
 
-export default class LandingScreen extends React.Component {
+class Landing extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      data_landing : [
+        {
+          asal: 'A',
+          waktu: '2016',
+          kode_pesawat: 'asc',
+          penumpang: 126
+        },
+        {
+          asal: 'B',
+          waktu: '2017',
+          kode_pesawat: 'das',
+          penumpang: 324
+        }
+      ]
+    };
+  }
+
   render(){
     return (
       <View>
-        <Text>Menu landing</Text>
+        <List>
+          {
+            this.state.data_landing.map((l, i) => (
+              <ListItem
+                key={i}
+                title={l.asal}
+                subtitle={l.kode_pesawat}
+              />
+            ))
+          }
+        </List>
+        
       </View>
     );
   }
 }
+
+const LandingScreen = createStackNavigator({
+  Landing: {
+    screen: Landing,
+    navigationOptions: {
+      title: 'Data Landing'
+    }
+  }
+});
+
+export default LandingScreen;
