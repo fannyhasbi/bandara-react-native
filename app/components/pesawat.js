661d37d7d1582fb9ecd315ea138640c9eabe3f22
fannyhasbi <fnyhsbi@gmail.com>
2018-05-28 22:42:14 +0700

    Update pesawat screen

--- app/components/pesawat.js
+++ app/components/pesawat.js
@@ -1,20 +1,41 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import {List, ListItem, Button, FormLabel, FormInput} from 'react-native-elements';
+import { Text, View, ScrollView, StyleSheet } from 'react-native';
+import {List, ListItem, Button, FormLabel, FormInput, Divider} from 'react-native-elements';
 import {createStackNavigator} from 'react-navigation';
+
+import DetailPesawat from './pesawat/detailpesawat';
 
 class Pesawat extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      data_pesawat : [
+      datas : [
         {
-          nama: 'A',
-          kode: 'abd'
+          kode: 'PXAXO',
+          nama: 'Airbus A320-200',
+          kode_maskapai: 'A001',
+          maskapai: 'AirAsia',
+          kapasitas: 180,
+          kode_pabrik: '0A01',
+          pabrik: 'Airbus'
         },
         {
-          nama: 'Boeing',
-          kode: 'cda'
+          kode: 'PKGLV',
+          nama: 'Boeing 737-300',
+          kode_maskapai: 'C001',
+          maskapai: 'Citilink',
+          kapasitas: 149,
+          kode_pabrik: '0A03',
+          pabrik: 'Boeing'
+        },
+        {
+          kode: 'PKGJK',
+          nama: 'Boeing 737-800',
+          kode_maskapai: 'G001',
+          maskapai: 'Garuda Indonesia',
+          kapasitas: 189,
+          kode_pabrik: '0A03',
+          pabrik: 'Boeing'
         }
       ]
     };
@@ -27,17 +48,32 @@ class Pesawat extends React.Component {
           title="Tambah"
           onPress={() => this.props.navigation.navigate('TambahPesawat')}
         />
-        <List>
-          {
-            this.state.data_pesawat.map((l, i) => (
-              <ListItem
-                key={i}
-                title={l.nama}
-                subtitle={l.kode}
-              />
-            ))
-          }
-        </List>
+
+        <Divider style={styles.batas} />
+
+        <ScrollView contentContainerStyle={styles.scroll}>
+          <List>
+            {
+              this.state.datas.map((l, i) => (
+                <ListItem
+                  key={i}
+                  title={l.nama}
+                  subtitle={l.maskapai}
+                  leftIcon={{name: 'flight'}}
+                  onPress={() => this.props.navigation.navigate('DetailPesawat', {
+                    kode: l.kode,
+                    nama: l.nama,
+                    kode_maskapai: l.kode_maskapai,
+                    maskapai: l.maskapai,
+                    kapasitas: l.kapasitas,
+                    kode_pabrik: l.kode_pabrik,
+                    pabrik: l.pabrik
+                  })}
+                />
+              ))
+            }
+          </List>
+        </ScrollView>
         
       </View>
     );
@@ -91,12 +127,26 @@ const PesawatScreen = createStackNavigator({
     navigationOptions: {
       title: 'Tambah Pesawat'
     }
-  }
+  },
+  DetailPesawat: {
+    screen: DetailPesawat,
+    navigationOptions: {
+      title: 'Detail Pesawat'
+    }
+  } 
 });
 
 const styles = StyleSheet.create({
   container: {
     paddingTop: 20
+  },
+  batas: {
+    marginTop: 20,
+    paddingTop: 0,
+    height: 0
+  },
+  scroll: {
+    paddingVertical: 0
   }
 });
 
