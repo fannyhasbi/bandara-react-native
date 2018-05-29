2d52bd8b94de90d0dde807d0717b0d0cdce320d9
fannyhasbi <fnyhsbi@gmail.com>
2018-05-29 13:31:55 +0700

    Update landing screen

--- app/components/landing.js
+++ app/components/landing.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView } from 'react-native';
 import {List, ListItem} from 'react-native-elements';
 import {createStackNavigator} from 'react-navigation';
 
@@ -7,18 +7,33 @@ class Landing extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      data_landing : [
+      datas : [
         {
-          asal: 'A',
-          waktu: '2016',
-          kode_pesawat: 'asc',
-          penumpang: 126
+          kode_pesawat: 'PKCJC',
+          pesawat: 'Boeing 737-300',
+          maskapai: 'Sriwijaya Air',
+          asal: 'Balikpapan',
+          bandara: 'Bandar Udara Internasional Sultan Aji Muhammad Sulaiman',
+          penumpang: 130,
+          waktu: '23 Nov \'17 10:19',
         },
         {
-          asal: 'B',
-          waktu: '2017',
-          kode_pesawat: 'das',
-          penumpang: 324
+          kode_pesawat: 'PKLGQ',
+          pesawat: 'Boeing 737-800',
+          maskapai: 'Lion Air',
+          asal: 'Pangkalanbun',
+          bandara: 'Bandar Udara Iskandar',
+          penumpang: 143,
+          waktu: '23 Nov \'17 08:16',
+        },
+        {
+          kode_pesawat: 'PKCJC',
+          pesawat: 'Boeing 737-300',
+          maskapai: 'Sriwijaya Air',
+          asal: 'Balikpapan',
+          bandara: 'Bandar Udara Internasional Sultan Aji Muhammad Sulaiman',
+          penumpang: 97,
+          waktu: '8 Nov \'17 03:22',
         }
       ]
     };
@@ -26,20 +41,21 @@ class Landing extends React.Component {
 
   render(){
     return (
-      <View>
+      <ScrollView>
         <List>
           {
-            this.state.data_landing.map((l, i) => (
+            this.state.datas.map((l, i) => (
               <ListItem
                 key={i}
-                title={l.asal}
-                subtitle={l.kode_pesawat}
+                title={l.maskapai + ' - ' + l.pesawat}
+                subtitle={l.asal}
+                leftIcon={{name: 'flight-land'}}
               />
             ))
           }
         </List>
         
-      </View>
+      </ScrollView>
     );
   }
 }
